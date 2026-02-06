d01a24d0a1d2e41ae50ba03b18f3e71ae0a24ccb
Michael <mjb2632@utulsa.edu>
2026-02-05 20:07:27 -0600

    Fix printf statements in print_name

--- print_name.c
+++ print_name.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) printf("No name provided as input");
-    else if (argc > 2) printf("Too many arguments provided as input");
+    if (argc < 2) printf("No name provided as input\n");
+    else if (argc > 2) printf("Too many arguments provided as input\n");
     else {
-        printf(argv[1]);
+        printf("%s", argv[1]);
     }
     return 0;
 }
