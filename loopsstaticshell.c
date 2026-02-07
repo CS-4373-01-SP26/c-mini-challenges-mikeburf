4e7b35568f1e283d2104438d4b9609c36cb538dc
Michael <mjb2632@utulsa.edu>
2026-02-07 10:43:31 -0600

    Add iteration to both loop programs

--- loopsstaticshell.c
+++ loopsstaticshell.c
@@ -1,32 +1,39 @@
 #include <time.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-  int i,j;
+int main(int argc, char **argv)
+{  
   int n = 128;
-  double sum;
-  clock_t end, start;
-  double arr[128][128];
-  
-// THIS FILLS THE MATRIX WITH NUMBERS
-  for (i=0; i<n; i++)
-    for (j=0; j<n; j++)
-      arr[i][j] = (double) rand()/RAND_MAX;
-
-  sum = 0;
-
-// ROW MAJOR WORK
-// YOU'LL NEED TO TIME IT
-for (i = 0; i<n; i++) // iterate over rows 
-  for (j = 0; j<n; j++) // iterate over columns 
-    sum += arr[i][j];
-
-// NOTE:  YOU'LL NEED TO PROVIDE MEANING TO end AND start
-  printf("Row Major: sum = %lf and Clock Ticks are %ld\n",sum,end-start);
-
-//ADD YOUR COLUMN MAJOR WORK
-// YOU'LL NEED TO TIME IT
-
+  for (int i = 0; i < 3; i++) {
+    work(n);
+    n *= 2;
+  }
   return 0;
 }
 
+void work(int n)
+{
+  int i, j;
+  double sum;
+  clock_t end, start;
+  double arr[128][128];
+
+  // THIS FILLS THE MATRIX WITH NUMBERS
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      arr[i][j] = (double)rand() / RAND_MAX;
+
+  sum = 0;
+
+  // ROW MAJOR WORK
+  // YOU'LL NEED TO TIME IT
+  for (i = 0; i < n; i++)   // iterate over rows
+    for (j = 0; j < n; j++) // iterate over columns
+      sum += arr[i][j];
+
+  // NOTE:  YOU'LL NEED TO PROVIDE MEANING TO end AND start
+  printf("Row Major: sum = %lf and Clock Ticks are %ld\n", sum, end - start);
+
+  // ADD YOUR COLUMN MAJOR WORK
+  //  YOU'LL NEED TO TIME IT
+}
